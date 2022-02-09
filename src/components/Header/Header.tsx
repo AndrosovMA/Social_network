c5ea5e32c36d3c8ebd4b2246fe439e9a70d4d95d
AndrosovMA <AndrosovMA@bk.ru>
2022-02-09 14:28:20 +0300

    Initialize project using Create React App
    Initialize project using Create React App

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+export function Header() {
+    console.log('header');
+
+    return (
+        <>
+            <div>Header</div>
+        </>
+    )
+}
+
+
+function proba() {
+    console.log('proba')
+
+
+
+}
+proba();
+
+
+let a: {
+    value: Number,
+};
+
+a = {
+    value: 345345,
+}
+
+console.log(a)

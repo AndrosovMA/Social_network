65f074a9df5bdf78430d05c4436d54de404549a5
AndrosovMA <AndrosovMA@bk.ru>
2022-02-12 18:28:27 +0300

    v.0.2.2 - add NavLink in Dialogs.tsx

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,7 +1,34 @@
+import {NavLink} from "react-router-dom";
+
 const Dialogs = () => {
     return (
         <div className='dialogs'>
-            <div>dialogs</div>
+
+            <div className="dialogs__users">
+                <div className="user__name active">
+                    <NavLink to='1'>Max</NavLink>
+                </div>
+                <div className="user__name">
+                    <NavLink to='2'>Ivan</NavLink>
+                </div>
+                <div className="user__name">
+                    <NavLink to='3'>Georgy</NavLink>
+                </div>
+                <div className="user__name">
+                    <NavLink to='4'>Olga</NavLink>
+                </div>
+            </div>
+
+
+            <div className="messages__users">
+                <div className="user__message">
+                    happy coding
+                </div>
+                <div className="user__message">
+                    and good hacking
+                </div>
+            </div>
+
          </div>
     )
 }
